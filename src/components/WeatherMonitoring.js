2a2b0299e3215c0fd748ae4955347150cac6c40c
blitzRahul <104504393+blitzRahul@users.noreply.github.com>
2023-12-15 14:59:17 +0530

    weather

--- src/components/WeatherMonitoring.js
+++ src/components/WeatherMonitoring.js
@@ -2,116 +2,119 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { useRef } from 'react';
-import {
+import {Line} from 'react-chartjs-2';
+import{
   Chart as ChartJS,
+  LineElement,
   CategoryScale,
   LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
+  PointElement
 } from 'chart.js';
-import { Line } from 'react-chartjs-2';
+
 
 ChartJS.register(
+  LineElement,
   CategoryScale,
   LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
+  PointElement
 );
+
+
+
+
 
 const WeatherMonitoring = () => {
 
 function handleClick(){
 
 }
-const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top',
-    },
-    title: {
-      display: true,
-      text: 'Chart.js Line Chart',
-    },
-  },
-};
 
-const we = [];
-let time=[];
-const [stringData,setStringData] = useState([]);
-const [timeData,setTimeData]  =useState([]);
-const [wData,setwData] = useState({
-  we,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: we,
-      borderColor: 'rgb(255, 99, 132)',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-  
-  ],
+
+
+
+
+const[data,setData] = useState({
+
+  labels:['mon','tue','thu'],
+  datasets:[{
+    labels:'wee',
+    data:[3,1,2],
+    backgroundColor:'red',
+    borderColor:'black',
+    pointBorderColor:'red',
+  }],
 });
+
+const options={
+  plugins:{
+    legends:true
+  },
+  scales:{
+   
+  }
+}
+
+
+
+
+
+
+
+
 
 useEffect(function(){
 
 
-
-
-
-
-
-  let temp=[];
   
     fetch("https://api.open-meteo.com/v1/forecast?latitude=23.2547&longitude=77.4029&hourly=temperature_2m")
     .then(res => res.json())
     .then((dt)=>{
       //code here to process the data
-      time = dt.hourly.time;
-      temp = dt.hourly.temperature_2m;
+      var time = dt.hourly.time;
+      var temperature = dt.hourly.temperature_2m;
       console.log(time);
-      console.log(temp );
+      console.log(temperature );
 
       time = time.map(function(item){
         let s = item.split("T");
         return(s[0]+" "+s[1]);
       });
+
+      temperature = temperature.map(function(element){return(element*1);});
  
 
+      setData({
 
-      setwData(  {
-        time,
-        datasets: [
-          {
-            label: 'Dataset 1',
-            data: temp.map(num=>num*1),
-            borderColor: 'rgb(255, 99, 132)',
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-          },
-        
-        ],
+        labels:time,
+        datasets:[{
+          labels:'wee',
+          data:temperature,
+          backgroundColor:'aqua',
+          borderColor:'black',
+          pointBorderColor:'aqua',
+        }],
       });
-
-      setStringData(temp);
-          setTimeData(time);
     });
 
 
 },[]);
 
+
+
+
+
+
+
+
   return (
     <div className='d-flex flex-column justify-content-between align-items-center'>
       <h2>Weather Monitoring</h2>
       {/* Real-time weather parameters and alerts UI */}
-      <div className=' w-25'>
-      <Line options={options} data={wData} />
-      {stringData.map((tmp,index)=><div>{tmp+" "+timeData[index]}</div>)}
+      <div className='graph-box'>
+      <Line
+      data={data}
+      options={options}
+      ></Line>
       </div>
      
     </div>
@@ -119,3 +122,7 @@ useEffect(function(){
 };
 
 export default WeatherMonitoring;
+
+
+
+
